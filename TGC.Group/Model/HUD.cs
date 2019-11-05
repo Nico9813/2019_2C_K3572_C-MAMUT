758412e993192218a43195d9ea462c4eeb75e85c
FredeHG <federicom503@gmail.com>
2019-11-04 21:54:19 -0300

    Arboles 2

--- TGC.Group/Model/HUD.cs
+++ TGC.Group/Model/HUD.cs
@@ -3,15 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TGC.Core.Direct3D;
-using TGC.Core.Input;
 using TGC.Core.Mathematica;
 using TGC.Core.Text;
 using TGC.Group.Sprites;
 using TGC.Group.Objetos;
-using TGC.Examples.Engine2D;
 
 namespace TGC.Group.Model
 {
